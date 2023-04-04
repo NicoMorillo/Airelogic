119d779e6cc1c3dd770423c59915163c6079f45e
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2023-04-04 13:17:46 +0100

    step 2 & 3

--- flask/server.py
+++ flask/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 
-var1 = os.environ['BLA']
+var1 = os.environ['NAME']
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, my name is ' + var1 
+
+@app.route('/Alice')
+def Alice():
+    return 'Hello, my name is Alice' 
+
+@app.route('/Bob')
+def Bob():
+    return 'Hello, my name is Bob'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
